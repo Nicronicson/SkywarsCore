d71f0de6999b65dee968a9794bc4592d85f0f801
Nico Nußbaum <niconuss123@gmail.com>
2021-04-18 18:30:31 +0200

    Initial commit

---
+++ src/SkywarsCore/Kit.java
@@ -0,0 +1,133 @@
+package SkywarsCore;
+
+import org.bukkit.inventory.ItemStack;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Kit {
+    protected ItemStack leftHand;
+    protected Map<String, Integer> leftHandENC;
+
+    protected ItemStack helmet;
+    protected Map<String, Integer> helmetENC;
+    protected ItemStack chestplate;
+    protected Map<String, Integer> chestplateENC;
+    protected ItemStack leggings;
+    protected Map<String, Integer> leggingsENC;
+    protected ItemStack boots;
+    protected Map<String, Integer> bootsENC;
+
+    protected ItemStack[] inventory;
+    protected ArrayList<Map<String, Integer>> inventoryENC;
+
+    public Kit(ItemStack leftHand, Map<String, Integer> leftHandENC, ItemStack helmet, Map<String, Integer> helmetENC, ItemStack chestplate, Map<String, Integer> chestplateENC, ItemStack leggings, Map<String, Integer> leggingsENC, ItemStack boots, Map<String, Integer> bootsENC, ItemStack[] inventory, ArrayList<Map<String, Integer>> inventoryENC) {
+        this.leftHand = leftHand;
+        this.leftHandENC = leftHandENC;
+        this.helmet = helmet;
+        this.helmetENC = helmetENC;
+        this.chestplate = chestplate;
+        this.chestplateENC = chestplateENC;
+        this.leggings = leggings;
+        this.leggingsENC = leggingsENC;
+        this.boots = boots;
+        this.bootsENC = bootsENC;
+        this.inventory = inventory;
+        this.inventoryENC = inventoryENC;
+    }
+
+    public ItemStack getLeftHand() {
+        return leftHand;
+    }
+
+    public void setLeftHand(ItemStack leftHand) {
+        this.leftHand = leftHand;
+    }
+
+    public Map<String, Integer> getLeftHandENC() {
+        return leftHandENC;
+    }
+
+    public void setLeftHandENC(Map<String, Integer> leftHandENC) {
+        this.leftHandENC = leftHandENC;
+    }
+
+    public ItemStack getHelmet() {
+        return helmet;
+    }
+
+    public void setHelmet(ItemStack helmet) {
+        this.helmet = helmet;
+    }
+
+    public Map<String, Integer> getHelmetENC() {
+        return helmetENC;
+    }
+
+    public void setHelmetENC(Map<String, Integer> helmetENC) {
+        this.helmetENC = helmetENC;
+    }
+
+    public ItemStack getChestplate() {
+        return chestplate;
+    }
+
+    public void setChestplate(ItemStack chestplate) {
+        this.chestplate = chestplate;
+    }
+
+    public Map<String, Integer> getChestplateENC() {
+        return chestplateENC;
+    }
+
+    public void setChestplateENC(Map<String, Integer> chestplateENC) {
+        this.chestplateENC = chestplateENC;
+    }
+
+    public ItemStack getLeggings() {
+        return leggings;
+    }
+
+    public void setLeggings(ItemStack leggings) {
+        this.leggings = leggings;
+    }
+
+    public Map<String, Integer> getLeggingsENC() {
+        return leggingsENC;
+    }
+
+    public void setLeggingsENC(Map<String, Integer> leggingsENC) {
+        this.leggingsENC = leggingsENC;
+    }
+
+    public ItemStack getBoots() {
+        return boots;
+    }
+
+    public void setBoots(ItemStack boots) {
+        this.boots = boots;
+    }
+
+    public Map<String, Integer> getBootsENC() {
+        return bootsENC;
+    }
+
+    public void setBootsENC(Map<String, Integer> bootsENC) {
+        this.bootsENC = bootsENC;
+    }
+
+    public ItemStack[] getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ItemStack[] inventory) {
+        this.inventory = inventory;
+    }
+
+    public ArrayList<Map<String, Integer>> getInventoryENC() {
+        return inventoryENC;
+    }
+
+    public void setInventoryENC(ArrayList<Map<String, Integer>> inventoryENC) {
+        this.inventoryENC = inventoryENC;
+    }
+}
---
+++ src/SkywarsCore/Map.java
@@ -0,0 +1,98 @@
+package SkywarsCore;
+
+import org.bukkit.Location;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Map {
+    protected String mapname;
+    protected Location middle;
+    protected Location pos1;
+    protected Location pos2;
+
+    protected List<Location> spawnpoints;
+    protected List<Location> chests;
+    protected List<Location> middleChests;
+
+    public Map(String mapname){
+        this.mapname = mapname;
+        this.middle = null;
+        this.pos1 = null;
+        this.pos2 = null;
+        this.spawnpoints = new ArrayList<>();
+        this.chests = new ArrayList<>();
+        this.middleChests = new ArrayList<>();
+    }
+
+    public void setMapname(String mapname) {
+        this.mapname = mapname;
+    }
+
+    public void setSpawnpoints(List<Location> spawnpoints) {
+        this.spawnpoints = spawnpoints;
+    }
+
+    public String getMapname(){
+        return mapname;
+    }
+
+    public List<Location> getSpawnpoints() {
+        return spawnpoints;
+    }
+
+    public List<Location> getChests() {
+        return chests;
+    }
+
+    public List<Location> getMiddleChests() {
+        return middleChests;
+    }
+
+    public Location getMiddle() {
+        return middle;
+    }
+
+    public void setMiddle(Location middle) {
+        this.middle = middle;
+    }
+
+    public void setPos1(Location pos1) {
+        this.pos1 = pos1;
+    }
+
+    public Location getPos1(){
+        return pos1;
+    }
+
+    public void setPos2(Location pos2) {
+        this.pos2 = pos2;
+    }
+
+    public Location getPos2(){
+        return pos2;
+    }
+
+    public void addSpawnpoint(Location spawnpoint){
+        spawnpoints.add(spawnpoint);
+    }
+
+    public void resetSpawnpoints(){
+        spawnpoints.clear();
+    }
+
+    public void setChests(List<Location> chests){
+        this.chests = chests;
+    }
+
+    public void setMiddleChests(List<Location> middleChests){
+        this.middleChests = middleChests;
+    }
+
+    public boolean positionsAvailable(){
+        if(pos1 == null || pos2 == null){
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
